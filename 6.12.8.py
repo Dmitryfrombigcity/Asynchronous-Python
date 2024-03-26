38837c859f5fd687ea902f241c0b3cf3e0318cd1
Dmitry Morozov <morozov124@gmail.com>
2024-03-27 00:27:47 +0300

    * Add 6.12.8.py

---
+++ 6.12.8.py
@@ -0,0 +1,44 @@
+import asyncio
+from contextvars import ContextVar, copy_context
+
+var: ContextVar = ContextVar('var')
+
+codes = [
+    "56FF4D", "A3D2F7", "B1C94A", "F56A1D", "D4E6F1",
+    "A1B2C3", "D4E5F6", "A7B8C9", "D0E1F2", "A3B4C5",
+    "D6E7F8", "A9B0C1", "D2E3F4", "A5B6C7", "D8E9F2"
+]
+
+messages = [
+    "Привет, мир!", "Как дела?", "Что нового?", "Добрый день!", "Пока!",
+    "Спокойной ночи!", "Удачного дня!", "Всего хорошего!", "До встречи!", "Счастливого пути!",
+    "Успехов в работе!", "Приятного аппетита!", "Хорошего настроения!", "Спасибо за помощь!",
+    "Всего наилучшего!"
+]
+
+
+def get_code(task: asyncio.Task) -> None:
+    code: str = var.get()
+    print(
+        f'{task.result()}' if int(code, 16) % 2 else
+        f'Сообщение: Неверный код, сообщение скрыто',
+        f'Код: {code}',
+        sep='\n'
+    )
+
+
+async def coro(message: str) -> str:
+    return f'Сообщение: {message}'
+
+
+async def main():
+    tasks = [asyncio.create_task(coro(message)) for message in messages]
+    for task, code in zip(tasks, codes):
+        var.set(code)
+        ctx = copy_context()
+        task.add_done_callback(get_code, context=ctx)
+    await asyncio.gather(*tasks)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
